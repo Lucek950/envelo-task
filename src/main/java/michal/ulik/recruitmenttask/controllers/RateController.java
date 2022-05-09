b3b56f77279533fe53739dec81622d30f87a7240
Michal Ulik <michal.ulik@ppuc.pl>
2022-05-09 22:49:47 +0200

    change controller annotation on restconraoller annotation

--- src/main/java/michal/ulik/recruitmenttask/controllers/RateController.java
+++ src/main/java/michal/ulik/recruitmenttask/controllers/RateController.java
@@ -3,14 +3,14 @@ package michal.ulik.recruitmenttask.controllers;
 import lombok.RequiredArgsConstructor;
 import michal.ulik.recruitmenttask.model.dtos.RateDto;
 import michal.ulik.recruitmenttask.services.RateService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/currency")
 public class RateController {
     private final RateService rateService;
